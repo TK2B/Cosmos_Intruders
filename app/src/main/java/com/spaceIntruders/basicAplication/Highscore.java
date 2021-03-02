af8ba08b430089841125772b4753d7ad2c9b69d6
Bastian Wegstein <bastianwegstein@MacBook-Pro-von-Bastian.local>
2021-03-02 22:02:44 +0100

    Initial commit - March_2_20201 - BW

---
+++ app/src/main/java/com/spaceIntruders/basicAplication/Highscore.java
@@ -0,0 +1,14 @@
+package com.spaceIntruders.basicAplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Highscore extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_highscore);
+    }
+}
