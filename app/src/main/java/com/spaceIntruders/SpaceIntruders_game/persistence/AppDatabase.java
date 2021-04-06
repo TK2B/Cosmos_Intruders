dc5377ca42ff995cd8ae6002d649ec9ca12632d1
tk2b <bastian.wegstein@gmail.com>
2021-04-07 00:32:43 +0200

    implemented init of Database

--- app/src/main/java/com/spaceIntruders/SpaceIntruders_game/persistence/AppDatabase.java
+++ app/src/main/java/com/spaceIntruders/SpaceIntruders_game/persistence/AppDatabase.java
@@ -1,9 +1,37 @@
 package com.spaceIntruders.SpaceIntruders_game.persistence;
 
+import android.content.Context;
+import android.provider.SyncStateContract;
+
 import androidx.room.Database;
+import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
 @Database(entities = {Player_user.class}, version = 1)
 public abstract class AppDatabase extends RoomDatabase {
-    public abstract DAO_player_user userDao();
+    private static final String TABLE_NAME_PLAYER_USER = "myPlayerDatebase";
+    public abstract DAO_player_user getPlayer_userDao;
+
+
+    private static AppDatabase Player_DB;
+
+
+    public static AppDatabase getInstance(Context context) {
+        if (null == Player_DB) {
+            Player_DB = buildDatabaseInstance(context);
+        }
+        return Player_DB;
+    }
+
+    private static AppDatabase buildDatabaseInstance(Context context) {
+        return Room.databaseBuilder(context,
+                AppDatabase.class,
+               TABLE_NAME_PLAYER_USER)  // TODO MAke Constant for DAO and Here
+                .allowMainThreadQueries().build();
+    }
+
+    public void cleanUp(){
+        AppDatabase = null;
+    }
+
 }
--- app/src/main/java/com/spaceIntruders/SpaceIntruders_game/persistence/DAO_player_user.java
+++ app/src/main/java/com/spaceIntruders/SpaceIntruders_game/persistence/DAO_player_user.java
@@ -1,15 +1,25 @@
 package com.spaceIntruders.SpaceIntruders_game.persistence;
 
+import android.provider.SyncStateContract;
+
 import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.Query;
+import androidx.room.Update;
 
 import java.util.List;
 import com.spaceIntruders.SpaceIntruders_game.persistence.Player_user;
+import com.spaceIntruders.SpaceIntruders_game.persistence.AppDatabase;
+
+
 
 @Dao
 public interface DAO_player_user {
+    @Query(value = "SELECT * FROM myPlayerDatebase")   //ToDO make sure it exists manual https://www.pluralsight.com/guides/making-a-notes-app-using-room-database
+    List <Player_user> getAll();
+
+
     @Query("SELECT * FROM Player_user ORDER BY highScore ASC")
     List <Player_user> getAll();
 
@@ -19,9 +29,31 @@ public interface DAO_player_user {
     @Query("SELECT * FROM Player_user WHERE first_name LIKE :first LIMIT 1")
     Player_user findByName(String first);
 
+    /*
+     * Insert the object in database
+     * @param note, object to be inserted
+     */
     @Insert
-    void insertAll(Player_user... users);
-
+    void insertAll(Player_user users);
+    /*
+     * delete the object from database
+     * @param note, object to be deleted
+     */
     @Delete
     void delete(Player_user user);
+
+    /*
+     * delete list of objects from database
+     * @param note, array of objects to be deleted
+     */
+    @Delete
+    void delete(Player_user... userS);      // Player_user... is varargs, here note is an array
+
+
+    /*
+     * update the object in database
+     * @param note, object to be updated
+     */
+    @Update
+    void update(Player_user repos);
 }
