4a65417a7f68c34de0bd9858ebb482df3920dfb5
tk2b <bastian.wegstein@gmail.com>
2021-04-07 00:07:20 +0200

    added getter setter and constructor to Player_user Class

--- app/src/main/java/com/spaceIntruders/SpaceIntruders_game/persistence/Player_user.java
+++ app/src/main/java/com/spaceIntruders/SpaceIntruders_game/persistence/Player_user.java
@@ -8,12 +8,37 @@ import androidx.room.PrimaryKey;
 @Entity
 public class Player_user {
     @PrimaryKey
-    public int uid;
+    private int uid;
 
     @ColumnInfo(name = "first_name")
-    public String firstName4db;
+    private String firstName4db;
 
     @ColumnInfo(name = "highScore")
-    public int highScore4db ;
+    private  int highScore4db ;
 
+    public Player_user(int uid, String firstName4db, int highScore4db) {
+        this.uid = uid;
+        this.firstName4db = firstName4db;
+        this.highScore4db = highScore4db;
+    }
+
+    public int getHighScore4db() {
+        return highScore4db;
+    }
+
+    public void setHighScore4db(int highScore4db) {
+        this.highScore4db = highScore4db;
+    }
+
+    public String getFirstName4db() {
+        return firstName4db;
+    }
+
+    public int getUid() {
+        return uid;
+    }
+
+   // TODO something like an override toString() (vor printing )
+
+    }
 }
